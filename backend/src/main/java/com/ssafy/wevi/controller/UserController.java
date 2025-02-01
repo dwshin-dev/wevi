4b1342a7f55a029fc2ff1b367adcb4cd897ce726
정윤선 <99yunsun@naver.com>
2025-02-01 23:59:56 +0900

    fix: Change user API endpoint

--- backend/src/main/java/com/ssafy/wevi/controller/UserController.java
+++ backend/src/main/java/com/ssafy/wevi/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/user")
 @RequiredArgsConstructor
 @Slf4j
 public class UserController {
