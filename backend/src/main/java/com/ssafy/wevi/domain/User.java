c7ce976d8e2e168b6381a0cebcedeb1bf2d9e011
신동운 <tlsehd5454@naver.com>
2025-02-04 15:29:58 +0900

    fix: change database user to users

--- backend/src/main/java/com/ssafy/wevi/domain/User.java
+++ backend/src/main/java/com/ssafy/wevi/domain/User.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 
 // JPA가 Entity에는 Getter, Setter, NoArgs 붙이라고 규칙을 정함.
 @Entity
-@Table(name = "user")  // 안붙이면 소문자 + snake_case. 이 경우에는 안써도 똑같음
+@Table(name = "users")  // 안붙이면 소문자 + snake_case. 이 경우에는 안써도 똑같음
 @Getter
 @Setter
 @NoArgsConstructor
