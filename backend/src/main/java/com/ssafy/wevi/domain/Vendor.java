3c688a096c834a93c9d4245d29077335d891933e
정윤선 <99yunsun@naver.com>
2025-02-06 17:13:38 +0900

    refactor: create Customer domain and fix User/Vendor domain

--- backend/src/main/java/com/ssafy/wevi/domain/Vendor.java
+++ backend/src/main/java/com/ssafy/wevi/domain/Vendor.java
@@ -12,19 +12,11 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
-public class Vendor {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer vendorId;   // 업체 ID
-
-    @Column(unique = true, nullable = false)
-    private String email;   // 이메일
+@DiscriminatorValue("vendor")
+public class Vendor extends User {
 
     @Column(nullable = false)
-    private String password;    // 비밀번호
-
-    @Column(nullable = false)
-    private String ownername;   // 대표자명
+    private String ownerName;   // 대표자명
 
     @Column(nullable = false)
     private String ownerPhone;  // 대표자 연락처
@@ -35,59 +27,55 @@ public class Vendor {
     @Column(nullable = false)
     private String zonecode;    // 우편번호
 
+//    @Column(nullable = false)
+//    private Sido sido; // 시도 - 외래키 연결 예정
+//
+//    @Column(nullable = false)
+//    private Sigungu sigungu;  //시군구 - 외래키 연결 예정
+
     @Column(nullable = false)
-    private Integer sidoId; // 시도ID
+    private String autoRoadAddress;  // 도로명주소
 
-    //    @Column(nullable = false)
-    private Integer sigunguId;  //시군구ID
+    @Column(nullable = false)
+    private String addressDetail;  // 세부 주소
 
-    //    @Column(nullable = false)
-    private String address; // 세부주소
-
-    //    @Column(nullable = false)
+    @Column(nullable = false)
     private String phone;   // 업체 연락처
 
-    //    @Column(nullable = false)
+    @Column(nullable = false)
     private String registrationNumber;  // 사업자등록번호
 
-    //    @Column(nullable = false)
-    private LocalDateTime createdAt;    // 생성일
+//    @Column(nullable = false)
+//    private Category category; // 카테고리 - 외래키 연결 예정
 
-    //    @Column(nullable = false)
-    private LocalDateTime updatedAt;    // 수정일
+    @Column(nullable = false)
+    private String businessHour;  // 영업시간
 
-    //    @Column(nullable = false)
-    private Integer categoryId; // 카테고리ID
-
-    //    @Column(nullable = false)
-    private String pictures;    // 업체사진
-
-    //    @Column(nullable = false)
-    private LocalDateTime businessStartTime; // 영업 시작시간
-
-    //    @Column(nullable = false)
-    private LocalDateTime businessEndTime;  // 영업 종료시간
-
-    //    @Column(nullable = false)
-    private String url; // 홈페이지 URL
+    @Column(nullable = true)
+    private String homepage; // 홈페이지 URL
 
     /**
      * 옵션별 가격 : String
      * {옵션명|가격|옵션명|가격|...}
      * 띄어쓰기 X, 가격은 세자리마다 쉼표(,)
      */
-//    @Column(nullable = false)
+    @Column(nullable = false)
     private String price;   // 옵션별 가격
 
-    //    @Column(nullable = false)
+    @Column(nullable = true)
     private String details; // 상세설명
 
-    //    @Column(nullable = false)
+    @Column(nullable = false)
     private boolean isIndoor;   // 실내/야외 여부
 
-    //    @Column(nullable = false)
-    private int min_price;  // 최소가격
+    @Column(nullable = false)
+    private int minPrice;  // 최소가격
 
+    @Column(nullable = false)
+    private int subway;  // 가까운 지하철역
+
+    @Column(nullable = false)
+    private int parkinglot;  // 주차 정보
 //    // 양방향 연결
 //    @OneToMany(mappedBy = "vendor")
 //    private List<Schedule> schedules;
