9aa5a5812186fb6dfe37dd7adc4eecaeb655b0e3
신동운 <tlsehd5454@naver.com>
2025-02-09 16:30:52 +0900

    feat: Add vendor dummy data

--- backend/src/main/java/com/ssafy/wevi/domain/Vendor.java
+++ backend/src/main/java/com/ssafy/wevi/domain/Vendor.java
@@ -48,7 +48,7 @@ public class Vendor extends User {
     @Column(nullable = false)
     private String registrationNumber;  // 사업자등록번호
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id")
     private Category category; // 카테고리 - 외래키
 
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -55,9 +55,33 @@ INSERT INTO `sigungu` (sigungu_id, do_id, sigungu_name) VALUES
     -- 제주도 (do_region_id = 39)
     (1, 39, '남제주군'), (2, 39, '북제주군'), (3, 39, '서귀포시'), (4, 39, '제주시');
 
+INSERT INTO `categories` VALUES (1, '예식장'), (2, '스튜디오'), (3, '드레스'), (4, '메이크업');
 
-INSERT INTO `users` VALUES (1,'2025-02-08 12:38:34.630324',NULL,'customer',NULL,NULL,'test@test.com','$2a$10$kRY19qCSG/Dt8CQYzt06YuACQgxLzvMG3GviCOHVKZLPh1bflbcPi','ACTIVE');
+INSERT INTO `users` (
+    user_id, created_at, updated_at, dtype, auth_id, auth_provider, email, password, status
+) VALUES
+      (1,'2025-02-08 12:38:34.630324',NULL,'customer',NULL,NULL,'test@test.com','$2a$10$kRY19qCSG/Dt8CQYzt06YuACQgxLzvMG3GviCOHVKZLPh1bflbcPi','ACTIVE'),
+      (2,'2025-02-09 07:15:56.089971',NULL,'vendor',NULL,NULL,'weddinghall1@test.com','$2a$10$7yYA5GzV0Z8g1uQ/B91Gd.S4LxIg5jzKmVXg7bJOLX9evY1OEHGUm','ACTIVE'),
+      (3,'2025-02-09 07:16:04.319151',NULL,'vendor',NULL,NULL,'weddinghall2@test.com','$2a$10$Mm9/rEzAMw8mMiNjmtO.QeqIM4NFNxeXaoTgpSnRuB7DCdBfkbH/K','ACTIVE'),
+      (4,'2025-02-09 07:16:10.844163',NULL,'vendor',NULL,NULL,'studio1@test.com','$2a$10$6JIYC5foDU.EZ1hsOcQZeuI0LWHrLPeq1eHfRXvrGXoejjbNxD0FO','ACTIVE'),
+      (5,'2025-02-09 07:16:16.561169',NULL,'vendor',NULL,NULL,'dress1@test.com','$2a$10$JMQTGnLmjAyeoGILmk8vbue1BLsYjPYYuU4CSnRMYSS3x2MisMxKi','ACTIVE'),
+      (6,'2025-02-09 07:16:28.266947',NULL,'vendor',NULL,NULL,'makeup1@test.com','$2a$10$jft0KaGIiJnMKQX2c6WJ1uYPJeaZKEDTuufVbQJsQvK0eYjvyxEF.','ACTIVE'),
+      (7,'2025-02-09 07:19:51.992539',NULL,'vendor',NULL,NULL,'studio2@test.com','$2a$10$FVSh.gO8oOah8LGGPaTdxOZttjam9vgrP4Yfg8ILiVCFyWLI/dBgi','ACTIVE'),
+      (9,'2025-02-09 07:20:56.299648',NULL,'vendor',NULL,NULL,'makeup2@test.com','$2a$10$bUO9mDoi/ABtze2g6hy2keCFrQRDjiJKqFsGBgVV4FtghladupoGW','ACTIVE'),
+      (10,'2025-02-09 07:21:10.449670',NULL,'vendor',NULL,NULL,'dress2@test.com','$2a$10$2LpSCbUmtBUyR2MezprunORJxC//a0OZ55pSiKZDr9Vi1TkVTfJXW','ACTIVE');
 
 INSERT INTO `customers` VALUES (NULL,1,'101동 101호','서울 양천구 목동서로1길 1','정윤선','쟈몽','01012345678','01234');
 
-INSERT INTO `categories` VALUES (1, '예식장'), (2, '스튜디오'), (3, '드레스'), (4, '메이크업');
+INSERT INTO `vendors` (
+    category_id, do_id, is_indoor, min_price, sigungu_code, user_id, address_detail, auto_road_address,
+    business_hour, details, homepage, name, owner_name, owner_phone, parkinglot, phone, price,
+    registration_number, subway, zonecode
+) VALUES
+      (1,1,_binary '\0',1000000,15,2,'5층','서울특별시 서초구 강남대로 123','10:00 - 22:00','럭셔리한 웨딩홀, 다양한 연회 메뉴 제공','http://www.lavenderwedding.com','라벤더 웨딩홀','박준영','010-1111-2222','주차 가능 100대','02-1234-5678','홀 대여 1,000,000원, 식대 50,000원','123-45-67890','강남역 2호선','06164'),
+      (1,2,_binary '\0',1200000,8,3,'2층','인천광역시 연수구 센트럴로 45','09:00 - 21:00','모던한 인테리어와 최신 음향 시설','http://www.grandwedding.com','그랜드 웨딩','이서연','010-2222-3333','주차 가능 80대','032-5678-1234','홀 대여 1,200,000원, 식대 55,000원','234-56-78901','인천대입구역 1호선','21431'),
+      (2,1,_binary '\0',500000,24,4,'B1','서울특별시 중구 남대문로 25','10:00 - 20:00','화이트 컨셉 촬영 전문 스튜디오','http://www.whitestudio.com','화이트 스튜디오','정민수','010-4444-5555','주차 가능 30대','02-9876-5432','촬영 패키지 500,000원','345-67-89012','서울역 1호선','04547'),
+      (3,31,_binary '\0',200000,23,5,'3층','경기도 용인시 수지구 포은대로 55','11:00 - 19:00','고급 맞춤 웨딩드레스 대여 및 판매','http://www.princessdress.com','프린세스 드레스','김하영','010-6666-7777','주차 가능 20대','031-2345-6789','드레스 대여 200,000원','456-78-90123','수지구청역 신분당선','41222'),
+      (4,6,_binary '\0',150000,16,6,'1층','부산광역시 해운대구 해운대로 789','10:00 - 18:00','신부 웨딩 메이크업 및 헤어 스타일링 전문','http://www.beautymakeup.com','뷰티 메이크업','박지현','010-8888-9999','주차 가능 10대','051-3456-7890','웨딩 메이크업 150,000원','567-89-01234','해운대역 2호선','48059'),
+      (2,2,_binary '\0',200000,3,7,'3층','인천광역시 미추홀구 예술로 456','10:00 - 20:00','감성적인 촬영을 제공하는 스튜디오입니다.','http://www.style-studio.com','스타일 스튜디오','이민호','010-3456-7890','전용 주차장 10대 가능','032-9876-1234','촬영 200,000원, 앨범 300,000원','210-56-12345','인천시청역 1호선','13579'),
+      (4,3,_binary '\0',100000,2,9,'2층','대전광역시 동구 중앙로 567','09:30 - 19:30','전문 아티스트가 맞춤 메이크업을 제공합니다.','http://www.beauty-makeup.com','박뷰티 메이크업','박예은','010-5678-9012','공용 주차장 이용 가능','042-1234-5678','신부 화장 150,000원, 일반 화장 100,000원','310-78-54321','대전역 1호선','54321'),
+      (3,4,_binary '\0',300000,1,10,'5층','대구광역시 남구 패션로 789','10:00 - 21:00','고급스러운 디자인의 웨딩 드레스 전문점입니다.','http://www.elegance-dress.com','엘레강스 드레스','김하늘','010-6789-0123','주차 타워 이용 가능','053-7890-1234','웨딩 드레스 500,000원, 턱시도 300,000원','410-98-76543','대구역 2호선','67890');
