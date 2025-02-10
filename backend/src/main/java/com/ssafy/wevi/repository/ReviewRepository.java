5fcb3b7cbce9689ff66df9d3ce0e162be5b9bd7a
신동운 <tlsehd5454@naver.com>
2025-02-10 14:18:35 +0900

    feat: Create ReviewRepository

---
+++ backend/src/main/java/com/ssafy/wevi/repository/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.ssafy.wevi.repository;
+
+import com.ssafy.wevi.domain.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Integer> {
+}
